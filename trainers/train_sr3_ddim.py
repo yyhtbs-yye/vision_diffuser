d752df34a1c8a78ee17e4ba27455b0a1723e7c8a
yyhtbs-yye <yye@research.ait.ie>
2025-05-04 16:29:20 +0000

    add sr3 ddim 16x128 config

--- trainers/train_sr3_ddim.py
+++ trainers/train_sr3_ddim.py
@@ -13,7 +13,7 @@ from datetime import timedelta
 
 torch.set_float32_matmul_precision('high')
 
-config_path = "configs/train_sr3_ddim_32_256.yaml"  # Updated config path
+config_path = "configs/train_sr3_ddim_16_128.yaml"  # Updated config path
 
 # Load YAML configuration
 with open(config_path, 'r') as f:
